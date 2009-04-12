7fe7b8a50a8c9416f4555012d75e8c1c70102fb0
tosh peter@home <toshism@gmail.com>
2009-04-12 11:09:03 -0500

    added media field to nzb model

--- django/birdpiss_nzb/nzb/models.py
+++ django/birdpiss_nzb/nzb/models.py
@@ -5,6 +5,9 @@ class Nzb(models.Model):
     newsgroup = models.CharField(blank=True, max_length=25)
     size = models.CharField(blank=True, max_length=10)
     xml_data = models.TextField(blank=False)
+    # this media field should probably be a foreignkey
+    # but as simple as it is right now meh....
+    media = models.CharField(blank=False, max_length=25)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
