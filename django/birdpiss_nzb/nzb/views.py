387d61f33db63ce27e1a603e631e3cc260de3bf1
tosh peter@home <toshism@gmail.com>
2009-04-12 19:46:46 -0500

    removed validation from upload view

--- django/birdpiss_nzb/nzb/views.py
+++ django/birdpiss_nzb/nzb/views.py
@@ -4,30 +4,28 @@ from nzb.models import Nzb
 
 def upload_nzb(request):
     if request.method == 'POST':
-        form = NzbUpload(request.POST, request.FILES)
-        if form.is_valid():
-            title = form.cleaned_data['title']
-            newsgroup = form.cleaned_data['newsgroup']
-            media = form.cleaned_data['media']
-            fsize = form.cleaned_data['size']
-            unit = form.cleaned_data['unit']
-            # put the two above together to stick in one field
-            size = "%s %s" % (fsize, unit)
+        title = request.POST['title']
+        newsgroup = request.POST['newsgroup']
+        media = request.POST['media']
+        fsize = request.POST['size']
+        unit = request.POST['unit']
+        # put the two above together to stick in one field
+        size = "%s %s" % (fsize, unit)
 
-            # get file contents to save with out saving file.
-            # in theory....
-            # i'm going to go ahead and guess that this is going
-            # to be a memory pig with large files. might need to
-            # rethink this...
-            usenet_file = form.cleaned_data.['usenet_file']
-            nzb_data = usenet_file.read()
+        # get file contents to save with out saving file.
+        # in theory....
+        # i'm going to go ahead and guess that this is going
+        # to be a memory pig with large files. might need to
+        # rethink this...
+        usenet_file = request.FILES['usenet_file']
+        nzb_data = usenet_file.read()
 
-            # save it
-            nzb = Nzb(title=title, newsgroup=newsgroup, media=media, size=size, xml_data=nzb_data)
-            nzb.save()
+        # save it
+        nzb = Nzb(title=title, newsgroup=newsgroup, media=media, size=size, xml_data=nzb_data)
+        nzb.save()
 
-            # return some form of success message here or something
-            return render_to_response('sucess.html')
+        # return some form of success message here or something
+        return render_to_response('sucess.html')
     else:
         form = NzbUpload()
 
