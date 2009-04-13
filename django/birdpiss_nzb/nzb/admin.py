eb7a3e85d4c1a89c40f9971781ba5fc086f9545b
Tosh <toshism@gmail.com>
2009-04-13 17:13:41 -0500

    cleaning up nzb admin

--- django/birdpiss_nzb/nzb/admin.py
+++ django/birdpiss_nzb/nzb/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from nzb.models import *
 
-admin.site.register(Nzb)
+class NzbAdmin(admin.ModelAdmin):
+    list_display = ('title', 'media', 'newsgroup', 'size',)
+    list_filter = ('media', 'newsgroup', )
+    search_fields = ['title']
+
+
+admin.site.register(Nzb, NzbAdmin)
