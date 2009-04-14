f7893d7977c3c11950c276e9609aa05463754911
Tosh <toshism@gmail.com>
2009-04-14 11:53:09 -0500

    added newsgroup lookup

--- django/birdpiss_nzb/nzb/views.py
+++ django/birdpiss_nzb/nzb/views.py
@@ -5,7 +5,6 @@ from nzb.models import Nzb
 def upload_nzb(request):
     if request.method == 'POST':
         title = request.POST['title']
-        newsgroup = request.POST['newsgroup']
         media = request.POST['media']
         fsize = request.POST['size']
         unit = request.POST['unit']
@@ -19,6 +18,8 @@ def upload_nzb(request):
         # rethink this...
         usenet_file = request.FILES['usenet_file']
         nzb_data = usenet_file.read()
+        
+        newsgroup = _get_newsgroup(nzb_data)
         
         # save it
         nzb = Nzb(title=title, newsgroup=newsgroup, media=media, size=size, xml_data=nzb_data)
@@ -41,3 +42,11 @@ def get_json(request, media):
 def dummy_json(request, media):
     
     return render_to_response('json/test.json',{'media':media},mimetype="application/json")
+
+
+def _get_newsgroup(n):
+    from xml.dom import minidom
+    
+    dom = minidom.parse(n)
+    node = dom.getElementsByTagName('group')[0]
+    return node.childNodes[0].data
