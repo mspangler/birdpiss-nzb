5bdd234cf81dd774301b65973340e9384479f8c5
mspangler <mspangler@gmail.com>
2009-04-16 07:12:30 -0700

    refactored the table ajax loader

--- web/js/application.js
+++ web/js/application.js
@@ -80,7 +80,7 @@ function setupTable() {
 	setupRowEvents();
 
 	// Create the ajax loader for table processing
-	$('#contentTbl_processing').attr('style', 'visibility:visible;').html('<img src="http://media.birdpiss.com/css/images/ajax-loader.gif" alt="Shovelling coal into the server..." />');
+	$('#contentTbl_processing').html('<img src="http://media.birdpiss.com/css/images/ajax-loader.gif" alt="Shovelling coal into the server..." />');
 
 	// Make the search textbox a little longer and add focus
 	$('#contentTbl_filter :text:first').addClass('ui-widget input nzbSearch').attr('size', 50).focus();
