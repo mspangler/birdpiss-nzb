a1cf5f0e6bc8ed6e31d3bfc5a80e3ead55f35774
Tosh <toshism@gmail.com>
2009-04-17 11:46:41 -0500

    added get oldest age method to nzbparser

--- django/birdpiss_nzb/nzb/nzbparser.py
+++ django/birdpiss_nzb/nzb/nzbparser.py
@@ -4,31 +4,27 @@ class NzbParser:
     
     def __init__(self, n):
         self.n = n
-    
-    def get_dom(self):
-        # returns the dom object
-        return xml.dom.minidom.parseString(self.n)
+        self.dom = xml.dom.minidom.parseString(self.n)
     
     def get_first_newsgroup(self):
-
-        dom = self.get_dom()
-        node = dom.getElementsByTagName('group')[0]
-
+        # gets the first newsgroup listed
+        # not good but close enough for me
+        node = self.dom.getElementsByTagName('group')[0]
+        
         return node.childNodes[0].data
     
     def get_size(self):
         # returns the size in bytes
-        dom = self.get_dom()
-        segments = dom.getElementsByTagName('segment')
-
+        segments = self.dom.getElementsByTagName('segment')
+        
         bytes = [int(node.attributes['bytes'].value) for node in segments]
-    
+        
         return sum(bytes)
-
+    
     def get_size_formatted(self):
         # returns a string formated size
         b = self.get_size()
-
+        
         kbs = b >> 10
         mbs = kbs >> 10
         gbs = mbs >> 10
@@ -38,5 +34,17 @@ class NzbParser:
             size = "%d %s" % (mbs, "MB")
         else:
             size = "%d %s" % (gbs, "GB")
-    
+        
         return size
+    
+    def get_oldest_age(self):
+        # get the age of oldest file in nzb
+        # return python datetime object
+        import datetime
+
+        files = self.dom.getElementsByTagName('file')
+        
+        ages = [node.attributes['date'].value for node in files]
+        dta = datetime.datetime.utcfromtimestamp(float(max(ages)))
+        
+        return dta
